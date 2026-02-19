55552c13aeb074f8ef2b754c1a3d643888654fb4
skyer <sky13net84@gmail.com>
2026-02-19 18:21:47 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,55 @@
+import java.util.Arrays;
 
-import  java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+        float[] arr2 = {1.57f, 7.654f, 9.986f};
+        int[] arr3 = new int[10];
+        for (int i = 0; i < arr3.length; i++) {
+            arr3[i] = i + 1;
+        }
+        System.out.println("\nЗадача 2");
+        for (int i = 0; i <= arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i == arr1.length - 1) {
+                break;
+            }
+            System.out.print(" , ");
+        }
+        System.out.println();
+        for (int i = 0; i <= arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i == arr1.length - 1) {
+                break;
+            }
+            System.out.print(" , ");
+        }
+        System.out.println();
+        for (int j = 0; j <= arr3.length; j++) {
+            System.out.print(arr3[j]);
+            if (j == arr3.length - 1) {
+                break;
+            }
+            System.out.print(" , ");
+        }
+        System.out.println("\nЗадача 3");
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
