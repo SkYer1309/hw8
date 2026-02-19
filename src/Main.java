76520e4cffb40829c5aca5dd556cdec65ecef2a4
skyer <sky13net84@gmail.com>
2026-02-19 20:37:30 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             arr3[i] = i + 1;
         }
         System.out.println("\nЗадача 2");
-        for (int i = 0; i <= arr1.length; i++) {
+        for (int i = 0; i < arr1.length; i++) {
             System.out.print(arr1[i]);
             if (i == arr1.length - 1) {
                 break;
@@ -20,7 +20,7 @@ public class Main {
             System.out.print(" , ");
         }
         System.out.println();
-        for (int i = 0; i <= arr2.length; i++) {
+        for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr2[i]);
             if (i == arr1.length - 1) {
                 break;
@@ -28,7 +28,7 @@ public class Main {
             System.out.print(" , ");
         }
         System.out.println();
-        for (int j = 0; j <= arr3.length; j++) {
+        for (int j = 0; j < arr3.length; j++) {
             System.out.print(arr3[j]);
             if (j == arr3.length - 1) {
                 break;
@@ -36,31 +36,60 @@ public class Main {
             System.out.print(" , ");
         }
         System.out.println("\nЗадача 3");
-        for (int i = 0; i <= arr1.length / 2; i++) {
+        for (int i = 0; i < arr1.length / 2; i++) {
             int x = arr1[i];
             arr1[i] = arr1[arr1.length - 1 - i];
             arr1[arr1.length - 1 - i] = x;
-                  }
-        System.out.print(Arrays.toString(arr1)+"\n");
-
-        for (int i = 0; i <= arr1.length / 2; i++) {
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i == arr1.length - 1) {
+                break;
+            }
+            System.out.print(" , ");
+        }
+        for (int i = 0; i < arr1.length / 2; i++) {
             float x = arr2[i];
             arr2[i] = arr2[arr1.length - 1 - i];
             arr2[arr1.length - 1 - i] = x;
         }
-        System.out.print(Arrays.toString(arr2)+"\n");
+        System.out.println();
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i == arr1.length - 1) {
+                break;
+            }
+            System.out.print(" , ");
+        }
+        System.out.println();
 
-        for (int i = 0; i <= arr1.length / 2; i++) {
+        for (int i = 0; i < arr1.length / 2; i++) {
             int x = arr3[i];
             arr3[i] = arr3[arr3.length - 1 - i];
             arr3[arr3.length - 1 - i] = x;
         }
-        System.out.print(Arrays.toString(arr3)+"\n");
-
-        System.out.println("\nЗадача 3");
-
+        for (int j = 0; j < arr3.length; j++) {
+            System.out.print(arr3[j]);
+            if (j == arr3.length - 1) {
+                break;
+            }
+            System.out.print(" , ");
+        }
+        System.out.println("\nЗадача 4");
+        for (int i = 0; i < arr1.length / 2; i++) {
+            int x = arr1[i];
+            arr1[i] = arr1[arr1.length - 1 - i];
+            arr1[arr1.length - 1 - i] = x;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] % 2 != 0) {
+                arr1[i] += 1;
+            }
+        }
+            System.out.print(Arrays.toString(arr1) + "\n");
+        }
     }
-}
+
 
 
 
