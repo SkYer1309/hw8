0577ed41960cdc4f5b6badd34cdbc7d5eeb5a2ba
skyer <sky13net84@gmail.com>
2026-02-20 07:53:14 +0300

    ДЗ доработано (2)

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         arr1[0] = 1;
         arr1[1] = 2;
         arr1[2] = 3;
-        float[] arr2 = {1.57f, 7.654f, 9.986f};
+        double[] arr2 = {1.57, 7.654, 9.986};
         int[] arr3 = new int[10];
         for (int i = 0; i < arr3.length; i++) {
             arr3[i] = i + 1;
@@ -14,7 +14,7 @@ public class Main {
         System.out.println("\nЗадача 2");
         for (int i = 0; i < arr1.length; i++) {
             System.out.print(arr1[i]);
-            if (i == arr2.length - 1) {
+            if (i == arr1.length - 1) {
                 break;
             }
             System.out.print(" , ");
@@ -22,7 +22,7 @@ public class Main {
         System.out.println();
         for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr2[i]);
-            if (i == arr1.length - 1) {
+            if (i == arr2.length - 1) {
                 break;
             }
             System.out.print(" , ");
@@ -48,22 +48,22 @@ public class Main {
             }
             System.out.print(" , ");
         }
-        for (int i = 0; i < arr1.length / 2; i++) {
-            float x = arr2[i];
-            arr2[i] = arr2[arr1.length - 1 - i];
-            arr2[arr1.length - 1 - i] = x;
+        for (int i = 0; i < arr2.length / 2; i++) {
+            double x = arr2[i];
+            arr2[i] = arr2[arr2.length - 1 - i];
+            arr2[arr2.length - 1 - i] = x;
         }
         System.out.println();
         for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr2[i]);
-            if (i == arr1.length - 1) {
+            if (i == arr2.length - 1) {
                 break;
             }
             System.out.print(" , ");
         }
         System.out.println();
 
-        for (int i = 0; i < arr1.length / 2; i++) {
+        for (int i = 0; i < arr3.length / 2; i++) {
             int x = arr3[i];
             arr3[i] = arr3[arr3.length - 1 - i];
             arr3[arr3.length - 1 - i] = x;
